0fcec59fc8bb08d7fd85637bbfbc7db07e3d2ed7
MURALIDHARAN M <84012752+PROMURALI77@users.noreply.github.com>
2025-03-10 16:53:54 +0000

    side panel & dashboard card added

--- ipl-auction/src/dashboard/Auction.jsx
+++ ipl-auction/src/dashboard/Auction.jsx
@@ -1,7 +1,14 @@
 
 const Auction = () => {
   return (
-    <div>Auction</div>
+    <div className="bg-darkgrey">
+        <div className="flex items-start justify-center h-screen font-poppins-medium text-white">
+            <div className="basis-3/4 flex items-center justify-center h-screen">
+            <div className="w-3/4 h-3/5 bg-litegrey rounded-xl">auction dashboard card</div>
+            </div>
+            <div className="basis-1/4 bg-white h-screen text-black">team rank side panel</div>
+        </div>
+    </div>
   )
 }
 
